01c1734137cbf0e537df80e9adb60ec0d3d7fc8a
Hu S.P <129255477+HuSP666@users.noreply.github.com>
2023-07-04 22:48:35 +0800

    Delete Gcd+ directory

--- Gcd/Gcd.py
+++ Gcd/Gcd.py
@@ -1,5 +1,5 @@
-a=int(input("1:"))
-b=int(input("2:"))
+﻿a=int(input("数字1是:"))
+b=int(input("数字2是:"))
 m=max(a,b)
 n=min(a,b)
 r=m%n
@@ -7,4 +7,4 @@ while (r!=0):
     m=n
     n=r
     r=m%n
-print("{},{}".format(n,int(a*b/n)))
+print("{}和{}".format(n,int(a*b/n)))
--- demo/demo.py
+++ demo/demo.py
@@ -1,1 +1,12 @@
-
+import time ###用前引入的申明
+while True:
+    print('who')
+    name=input()
+    if name !='HSP':
+        continue
+    print('password')
+    password=input()
+    if password =='king':
+        break
+print('welcome')
+time.sleep(10)  ###使用申明
