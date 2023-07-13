265bdcf9b491247fd25b6dafb55433bdc623e84d
Hu S.P <129255477+HuSP666@users.noreply.github.com>
2023-07-13 23:34:50 +0800

    Add files via upload

---
+++ Goat_Car_Door_Game.py
@@ -0,0 +1,35 @@
+﻿#但逻辑上似乎不大对：
+#from random import*
+#TIMES = 10000
+#my_first_choice_n=0	#初始化不改选择的次数
+#my_change_choice_n=0	#初始化更改选择的次数
+#for i in range(TIMES):
+#	car_inDoor=randint(0,2)
+#	my_guess=randint(0,2)
+#	if car_inDoor==my_guess:
+#		my_first_choice_n+=1
+#	else:
+#		my_change_choice_n+=1
+#print("不改选择:{}".format(my_first_choice_n/TIMES))
+#print("更改选择:{}".format(my_change_choice_n/TIMES))
+#
+
+import random
+Y=0
+N=0
+
+for i in range(100000):
+    car=random.randint(1,3)
+    try1=random.randint(1,3)
+    
+    if car==try1:
+        tryN=random.randint(1,3)
+        while tryN==try1:
+            tryN=random.randint(1,3)
+        N=N+1
+    else:
+        tryN=-1       #int("123".split(str(car),str(try1)))
+        Y=Y+1
+    print("{}but{}not{}".format(try1,car,tryN))
+
+print("Y={:%},N={:%}".format(Y/100000,N/100000))
